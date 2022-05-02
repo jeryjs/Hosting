fe94461ddd0e29209e75a16109a4ff13eee1f6e7
jeryjs-HP <jery99961@gmail.com>
2022-05-02 14:30:08 +0300

    improved script!

--- zMerge With Github.bat
+++ zMerge With Github.bat
@@ -1,8 +1,10 @@
 echo off
+echo adding "%1"
 git add "%1"
 echo Now enter the summary for this commit
 git commit
+echo commiting...
 git push
-echo success!!
-echo Exiting git
+echo push success!!
+echo on
 timeout /t 10

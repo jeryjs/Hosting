c13a493fe012f11a7c3836582effbfc39fec9e23
jeryjs-HP <jery99961@gmail.com>
2022-05-02 14:36:11 +0300

    3 changes
    Removed 3 ADM.txt, updated Readme,
    Improved script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This repo is purely for hosting scripts or storing other stuffs.
+This repo is purely for hosting scripts or storing other stuffs fo myself.
+There's nothing much for you to see here
--- zMerge With Github.bat
+++ zMerge With Github.bat
@@ -1,10 +1,13 @@
 echo off
 echo adding "%1"
+echo on
 git add "%1"
+echo off
 echo Now enter the summary for this commit
+echo on
 git commit
-echo commiting...
 git push
+echo off
 echo push success!!
 echo on
 timeout /t 10
